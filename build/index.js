acc27028091870bc00989cc677964ffaa6be8661
Sandra Quero <sanquesan@gmail.com>
2021-10-10 22:10:48 +0200

    Añadimos estilo tablero

--- build/index.js
+++ build/index.js
@@ -1,11 +1,8 @@
-// const tableroPequeño = document.getElementById("pequeño");
-// const tableroMedio = document.getElementById("medio");
-// const tableroGrande = document.getElementById("grande");
 const botonEmpezar = document.getElementById("empezar");
-const botonResetear = document.getElementById("Resetear");
+const botonResetear = document.getElementById("resetear");
 
-const columnas = 20;
-const filas = 20;
+const columnas = 30;
+const filas = 65;
 
 const contenedorTablero = document.querySelector(
   ".contenedor-principal__tablero"
@@ -13,123 +10,27 @@ const contenedorTablero = document.querySelector(
 const borrarTablero = () => {
   contenedorTablero.innerHTML = "";
 };
-// function crearFilasDivs() {
-//   const div = document.createElement("div");
-//   div.className = "contenedor-principal__filas";
-
-//   document.getElementById("contenedor-tablero").appendChild(div);
-// }
-// function alClicar(celula) {
-//   debugger;
-//   console.log("hola");
-//   celula.style.backgroundImage = "none";
-// }
-
-// function crearCelulas() {
-//   const celula = document.createElement("div");
-//   celula.className = "contenedor-principal__celula";
-
-//   const celulaIndividual = document.getElementsByClassName(
-//     "contenedor-principal__filas"
-//   );
-//   for (let i = 0; i < celulaIndividual.length; i++) {
-//     celulaIndividual[i].appendChild(celula);
-//   }
-//   celula.addEventListener("click", () => {
-//     celula.style.backgroundColor = "transparent";
-//   });
-// }
-
-// const crearTablero = (col, row) => {
-//   const primeraTabla = new Array(col);
-//   for (let i = 0; i < primeraTabla.length; i++) {
-//     primeraTabla[i] = new Array(row);
-//     for (let j = 0; j < primeraTabla[i].length; j++) {
-//       primeraTabla[i][j] = 0;
-//       crearCelulas();
-//     }
-//     crearFilasDivs();
-//   }
-//   return console.table(primeraTabla);
-// };
 
 const crearTablero = (col, row) => {
   const primeraTabla = new Array(col);
-  borrarTablero();
   const tablero = document.querySelector(".contenedor-principal__tablero");
 
   for (let i = 0; i < primeraTabla.length; i++) {
     primeraTabla[i] = new Array(row);
     let filasDivs = primeraTabla[i];
     filasDivs = document.createElement("div");
-    filasDivs.className = "contenedor-principal__filas";
+    filasDivs.classList.add("contenedor-principal__filas", i);
     for (let j = 0; j < primeraTabla[i].length; j++) {
-      primeraTabla[i][j] = 0;
-      let celulas = primeraTabla[i][j];
-      celulas = document.createElement("div");
-      celulas.className = "contenedor-principal__celula";
-      celulas.addEventListener("click", (elemento) => {
-        const clickCelda = elemento.target;
-        console.log(clickCelda.classList);
-        if (primeraTabla[i][j] === 0) {
-          primeraTabla[i][j] = 1;
-          celulas.classList.add("viva");
-          console.log(primeraTabla[i][j]);
-        } else {
-          primeraTabla[i][j] = 0;
-          celulas.classList.remove("viva");
-        }
-        console.table(primeraTabla);
-      });
+      const celulas = document.createElement("div");
+      primeraTabla[i][j] = celulas;
+      celulas.classList.add("contenedor-principal__celula", `${i}-${j}`);
+      celulas.addEventListener("click", (celda) => eventoClick(celda));
       filasDivs.appendChild(celulas);
     }
     tablero.appendChild(filasDivs);
   }
   return primeraTabla;
 };
-
-// function omitirBotonesTablero() {
-//   tableroGrande.style.display = "none";
-//   tableroPequeño.style.display = "none";
-//   tableroMedio.style.display = "none";
-// }
-
-// tableroPequeño.addEventListener("click", () => {
-//   columnas = 10;
-//   filas = 10;
-//   tamañoTablero = "pequeño";
-
-//   document.getElementById("contenedor-tablero").style.display = "flex";
-//   document.getElementById("contenedor-tablero").style.width = "200px";
-//   document.getElementById("contenedor-tablero").style.height = "200px";
-//   omitirBotonesTablero();
-
-//   crearTablero(columnas, filas);
-// });
-
-// tableroMedio.addEventListener("click", () => {
-//   columnas = 20;
-//   filas = 20;
-//   tamañoTablero = "medio";
-
-//   document.getElementById("contenedor-tablero").style.display = "flex";
-//   document.getElementById("contenedor-tablero").style.width = "400px";
-//   document.getElementById("contenedor-tablero").style.height = "400px";
-//   omitirBotonesTablero();
-//   crearTablero(columnas, filas);
-// });
-
-// tableroGrande.addEventListener("click", () => {
-//   columnas = 30;
-//   filas = 30;
-//   tamañoTablero = "grande";
-
-//   document.getElementById("contenedor-tablero").style.display = "flex";
-//   document.getElementById("contenedor-tablero").style.width = "600px";
-//   document.getElementById("contenedor-tablero").style.height = "600px";
-//   omitirBotonesTablero();
-//   crearTablero(columnas, filas);
-// });
 
 const contarVecinos = (tablero, col, row) => {
   let contador = 0;
@@ -168,27 +69,29 @@ const contarVecinos = (tablero, col, row) => {
 
 const nuevoTablero = (tablero) => {
   const primerTablero = tablero;
+  borrarTablero();
   const tableroNuero = crearTablero(columnas, filas);
 
   for (let i = 0; i < primerTablero.length; i++) {
     for (let j = 0; j < primerTablero[i].length; j++) {
       const vecinos = contarVecinos(primerTablero, j, i);
 
-      if (primerTablero[i][j] === 1 && vecinos < 2) {
-        tableroNuero[i][j] = 0;
-        tableroNuero[i][j].classList.remove("viva");
-      }
-      if (primerTablero[i][j] === 1 && (vecinos === 2 || vecinos === 3)) {
-        tableroNuero[i][j] = 1;
-        tableroNuero[i][j].classList.add("viva");
-      }
-      if (primerTablero[i][j] === 1 && vecinos > 3) {
-        tableroNuero[i][j] = 0;
-        tableroNuero[i][j].classList.remove("viva");
-      }
-      if (primerTablero[i][j] === 0 && vecinos === 3) {
-        tableroNuero[i][j] = 1;
-        tableroNuero[i][j].classList.add("viva");
+      if (primerTablero[i][j] === 1) {
+        if (vecinos === 2 || vecinos === 3) {
+          tableroNuero[i][j].classList.add("viva");
+          tableroNuero[i][j] = 1;
+        } else {
+          tableroNuero[i][j].classList.remove("viva");
+          tableroNuero[i][j] = 0;
+        }
+      } else if (primerTablero[i][j] === 0) {
+        if (vecinos === 3) {
+          tableroNuero[i][j].classList.add("viva");
+          tableroNuero[i][j] = 1;
+        } else {
+          tableroNuero[i][j].classList.remove("viva");
+          tableroNuero[i][j] = 0;
+        }
       }
     }
   }
@@ -203,32 +106,29 @@ for (let i = 0; i < tableroFinal.length; i++) {
   }
 }
 
+const eventoClick = (celda) => {
+  const celdaClicada = celda.target;
+  const posicion = celda.target.className.split(" ")[1].split("-");
+  const col = +posicion[0];
+  const row = +posicion[1];
+  if (celdaClicada.classList.contains("viva")) {
+    celdaClicada.classList.remove("viva");
+    tableroFinal[col][row] = 0;
+  } else {
+    celdaClicada.classList.add("viva");
+    tableroFinal[col][row] = 1;
+  }
+};
+
 botonEmpezar.addEventListener("click", () => {
   setInterval(() => {
     tableroFinal = nuevoTablero(tableroFinal);
-    console.table(tableroFinal);
-  }, 4000);
+  }, 300);
 });
 
 botonResetear.addEventListener("click", () => {
   location.reload();
-}
-// if (tamañoTablero === "pequeño") {
-//   tableroFinal = crearTablero(10, 10);
-// } else if (tamañoTablero === "medio") {
-//   tableroFinal = crearTablero(20, 20);
-// } else if (tamañoTablero === "grande") {
-//   tableroFinal = crearTablero(30, 30);
-// }
-
-// // tableroFinal[1][2] = 1;
-// // tableroFinal[2][2] = 1;
-// // tableroFinal[3][2] = 1;
-
-// setInterval(() => {
-//   tableroFinal = nuevoTablero(tableroFinal);
-//   console.table(tableroFinal);
-// }, 1000);
+});
 
 module.exports = {
   crearTablero,
